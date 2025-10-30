ee92672df9d40dde9f11795fc21fada04c3ab8fb
Bas Martens <bas.martens.1988@outlook.com>
2025-10-30 11:46:44 +0100

    feat: added source code to the project

---
+++ src/Calculator/Calculator.cs
@@ -0,0 +1,12 @@
+﻿namespace Calculator;
+
+public class Calculator
+{
+    public static int Add(int a, int b) => a + b;
+    
+    public static int Subtract(int a, int b) => a - b;
+    
+    public static int Multiply(int a, int b) => a * b;
+    
+    public static int Divide(int a, int b) => a / b;
+}
